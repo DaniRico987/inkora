d172d8642ad30a00bfab3c6ff68c1ff08843e9c2
Juan Jose Ruiz <juanjose.ruiz1@utp.edu.co>
2026-03-16 14:10:49 -0500

    feat: agregar nuevos componentes de entrada y mejorar estilos de botones, incluyendo gestión de temas y ajustes de diseño

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,62 @@
 
 import './App.css'
 import { Button } from './Components/Button'
-//import { Toggle } from './Components/Toggle'
+import { InputText,InputPassword,InputSelect,InputTextarea,Checkbox,InputSearch } from './Components/Inputs';
+import { Toggle } from './Components/Toggle'
 import { useTheme } from "./theme/useTheme";
 
 function App() {
   useTheme();
   return (
-    <div className="bg-white">
-      {/*<Toggle />*/}
-      <Button variant="primary" size='lg' onClick={() => console.log('Button clicked!')}>
+    <div className="bg-bg w-screen flex flex-col items-center justify-center gap-3.5 transition-all duration-300 ease-in-out">
+      {<Toggle />
+      }
+      <Button variant='primary' size="20rem" onClick={() => console.log('Primary button clicked!')}>
         Click me
       </Button>
+      <Button variant="secondary" size="20rem" onClick={() => console.log('Button clicked!')}>
+        Click me
+      </Button>
+      <Button variant="destructive" size="20rem" onClick={() => console.log('Destructive button clicked!')}>
+        Delete
+      </Button>
+
+      <div className="max-w-sm p-6 flex flex-col">
+        <InputText
+          label="Nombre completo"
+          value="Juan Pérez"
+        />
+
+        <InputText
+          label="Correo electrónico"
+          type="email"
+          placeholder="ejemplo@correo.com"
+        />
+
+        <InputPassword
+          label="Contraseña"
+          placeholder="Ingresa tu contraseña"
+        />
+
+        <InputTextarea
+          label="Descripción"
+          placeholder="Escribe aquí..."
+        />
+
+        <InputSelect
+          label="País"
+          options={[
+            { label: "Colombia", value: "co" },
+            { label: "México", value: "mx" },
+            { label: "Argentina", value: "ar" },
+          ]}
+        />
+
+        <Checkbox label="Acepto los términos y condiciones" />
+
+        <InputSearch placeholder="Buscar..." />
+      </div>
+
     </div>
   )
 }
--- client/src/Components/Button.tsx
+++ client/src/Components/Button.tsx
@@ -1,62 +1,50 @@
-import React from "react";
 
+import type { ButtonProps, Variant } from "../interfaces/ButtonInterface";
 
-type Variant = "primary" | "secondary" | "danger";
-type Size = "sm" | "md" | "lg";
-
-interface ButtonProps {
-  children: React.ReactNode;
-  onClick?: () => void;
-  variant?: Variant;
-  size?: Size;
-  disabled?: boolean;
-  loading?: boolean;
-  type?: "button" | "submit" | "reset";
-  className?: string;
-}
 
 const variantStyles: Record<Variant, string> = {
-  primary: 'bg-primary text-white hover:bg-blue-700',
-  secondary: "bg-skyblue text-white hover:bg-gray-300",
-  danger: "bg-red-600 text-white hover:bg-red-700",
+  primary: 'bg-primary-500 text-white',
+  secondary: "bg-babyblue-500 text-primary-500",
+  destructive: "bg-red-600 text-white",
 };
 
-const sizeStyles: Record<Size, string> = {
-  sm: "px-3 py-1 text-sm",
-  md: "px-4 py-2",
-  lg: "px-6 py-3 text-lg",
-};
 
 export function Button({
   children,
   onClick,
   variant = "primary",
-  size = "md",
+  size,
   disabled = false,
   loading = false,
   type = "button",
   className = "",
 }: ButtonProps) {
   return (
-    <button
-      type={type}
-      onClick={onClick}
-      disabled={disabled || loading}
-      className={`
+    <div className={`w-${'['+size+']' || 'full'}`}>
+      <button
+        type={type}
+        onClick={onClick}
+        disabled={disabled || loading}
+        className={`
         rounded-lg
-        font-medium
         transition
+        ease-in-out
         flex items-center gap-2
+        justify-center
+        py-2 px-4
+        w-full h-auto
+        text-[1rem]
+        hover:font-medium
         ${variantStyles[variant]}
-        ${sizeStyles[size]}
         ${disabled || loading ? "opacity-50 cursor-not-allowed" : ""}
         ${className}
       `}
-    >
-      {loading && (
-        <span className="animate-spin border-2 border-white border-t-transparent rounded-full w-4 h-4"></span>
-      )}
-      {children}
-    </button>
+      >
+        {loading && (
+          <span className="animate-spin border-2 border-white border-t-transparent rounded-full w-4 h-4"></span>
+        )}
+        {children}
+      </button>
+    </div>
   );
 }
