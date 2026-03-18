376e7e2080c0141ce11bdaf0287282f7bb79505c
Daniel Rico <daniel.rico@utp.edu.co>
2026-03-18 12:19:00 -0500

    feat: Integrate SnackbarProvider into App component for enhanced notification management

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,26 +4,36 @@ import { Toggle } from './Components/Toggle';
 import { useTheme } from './theme/useTheme';
 import { ForgotPasswordPage } from './pages/ForgotPasswordPage';
 import { ResetPasswordPage } from './pages/ResetPasswordPage';
+import { SnackbarProvider } from './Components/SnackbarProvider';
 
 function App() {
   useTheme();
   return (
     <BrowserRouter>
-      <div className="bg-bg w-screen min-h-screen flex flex-col transition-all duration-300 ease-in-out">
-        <header className="w-full flex justify-end p-4">
-          <Toggle />
-        </header>
-        <main className="flex-1 flex items-center justify-center">
-          <Routes>
-            <Route path="/forgot-password" element={<ForgotPasswordPage />} />
-            <Route
-              path="/reset-password/:token"
-              element={<ResetPasswordPage />}
-            />
-            {/* TODO: agregar aquí /login y demás rutas cuando estén listas */}
-          </Routes>
-        </main>
-      </div>
+      <SnackbarProvider
+        config={{
+          position: 'bottom-center',
+          maxVisible: 3,
+          maxQueue: 20,
+          dedupeWindowMs: 1500,
+        }}
+      >
+        <div className="bg-bg w-screen min-h-screen flex flex-col transition-all duration-300 ease-in-out">
+          <header className="w-full flex justify-end p-4">
+            <Toggle />
+          </header>
+          <main className="flex-1 flex items-center justify-center">
+            <Routes>
+              <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+              <Route
+                path="/reset-password/:token"
+                element={<ResetPasswordPage />}
+              />
+              {/* TODO: agregar aquí /login y demás rutas cuando estén listas */}
+            </Routes>
+          </main>
+        </div>
+      </SnackbarProvider>
     </BrowserRouter>
   );
 }
